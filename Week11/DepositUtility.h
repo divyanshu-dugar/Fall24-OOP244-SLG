63f268f0d5af1ed7db2848c6d3f30c70a816b6b1
Divyanshu Dugar <100013189+divyanshu-dugar@users.noreply.github.com>
2024-11-12 20:29:07 -0500

    Update DepositUtility.h

--- Week11/DepositUtility.h
+++ Week11/DepositUtility.h
@@ -1,16 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-						Workshop-7 Part-1
-Full Name  : Divyanshu Dugar
-Student ID#: 177110210
-Email      : ddugar@myseneca.ca
-Section    : ZAA
-Date       : 21/03/2024
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
-
 #ifndef SENECA_DEPOSIT_UTILITY_H
 #define SENECA_DEPOSIT_UTILITY_H
 
